c6f2d52682a0911fbce897b3b4d7c74aca2d0fbc
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-17 06:21:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,26 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
 
-  get '/' do
-     
-
+  post '/' do
+    erb :index
   end
+
+  get '/' do
+
+    erb :index
+  end
+
+  get '/new' do
+    erb:create_puppy
+  end
+
+  post '/puppy' do
+
+    erb:display_puppy
+  end
+
+
 
 end
